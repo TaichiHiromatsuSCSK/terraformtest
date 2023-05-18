f3ce188e187e18a5c3c7d6bf06ee0cf6c1fdbf95
TaichiHiromatsuSCSK <124754977+TaichiHiromatsuSCSK@users.noreply.github.com>
2023-05-18 20:26:37 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
-terraform {
-  required_providers {
-    google = "~> 3.70.0"
-  }
- 
-  provider google {
-    project = kaigo-fikapoc01
-    region = asia-northeast1
-  }
- 
- # state {
- #   backend = "gcs"
- #   bucket = var.state_bucket
- #   prefix = "terraform/state"
- # }
- 
-  resource "google_storage_bucket" "auto-expire" {
-  name          = "terraformpocfika-bucket"
-  location      = "ASIA"
-  force_destroy = true
-
-  public_access_prevention = "enforced"
-  }
+resource "google_storage_bucket" "bucket" {
+  name     = "fikapoc-tf-bucket"
+  location = "asia-northeast1"
 }
